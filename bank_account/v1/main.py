0031aca17f0232933c4ea44ed1e5a957abc92ca1
a-niazi <amir.niazi.2010@gmail.com>
2024-12-10 10:37:27 +0330

    clean code

--- bank_account/v1/main.py
+++ bank_account/v1/main.py
@@ -1,30 +1,30 @@
 from bank_account.v1.aggregates.account import Account
 from bank_account.v1.infrastructure.event_store import EventStore
 
-# ایجاد EventStore
+# Create EventStore
 event_store = EventStore()
 
-# ایجاد حساب
+# Create Account
 account_id = "acc123"
 account = Account(account_id)
 
 
-# واریز 1000 دلار به حساب
+# Deposit $1,000 into the account
 event1 = account.deposit(1000)
 event_store.save(account.account_id, [event1])
 
-# برداشت 500 دلار از حساب
+# Withdraw $500 from account
 event2 = account.withdraw(500)
 event_store.save(account.account_id, [event2])
 
-# مسدود کردن حساب
+# Block account
 event3 = account.block_account()
 event_store.save(account.account_id, [event3])
 
-# بازسازی وضعیت حساب
+# Reconstruct account status
 restored_account = Account.reconstruct(account_id, event_store)
 
-# مشاهده وضعیت حساب
+# View account status
 print(f"Account ID: {restored_account.account_id}")
 print(f"Balance: {restored_account.balance}")
 print(f"Blocked: {restored_account.blocked}")
